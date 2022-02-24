526715beeaaf0d8705fa6564965336094dc7f349
zaidmelhem <zaidomar2011426@gmail.com>
2022-02-24 16:03:05 +0200

    HW 1

---
+++ App3/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using App3.ViewModels;
+using App3.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ App3/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using App3.Models;
+using App3.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
