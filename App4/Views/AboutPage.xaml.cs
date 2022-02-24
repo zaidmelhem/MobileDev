6b42a9b7380502dc99b145f5be3121706a00a6ab
zaidmelhem <zaidomar2011426@gmail.com>
2022-02-24 16:04:10 +0200

    HW2

---
+++ App4/Views/AboutPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App4.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            MyWebView.Source = "https://zajel.najah.edu/";
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            MyWebView.Source = "https://www.google.com/";
+        }
+    }
+}
